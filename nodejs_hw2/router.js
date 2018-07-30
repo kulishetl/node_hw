d04443ccabec5d9a6d891c29bae1a751ce182521
kulishetl <kulishetl@gmail.com>
2018-07-30 19:26:03 +0300

    change config

--- nodejs_hw2/router.js
+++ nodejs_hw2/router.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const config = require("./config/config");
 const pizzaRouter = require("./routes/pizza");
 const orderRouter = require("./routes/order");
 const loginRouter = require("./routes/login");
 const registerRouter = require("./routes/register");
 const router = express.Router();
 
-router.use("/pizza", pizzaRouter);
-router.use("/order", orderRouter);
-router.use("/login", loginRouter);
-router.use("/register", registerRouter);
+const { pizza, order, login, register } = config.routes;
+router.use(pizza, pizzaRouter);
+router.use(order, orderRouter);
+router.use(login, loginRouter);
+router.use(register, registerRouter);
 
 module.exports = router;
